d3769fd22b92708f107b5392ae2e2e21f9d79c06
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 15:50:37 +0300

    Scripted auto-commit on change (2019-10-07 15:50:37) by gitwatch.sh

--- src/main/java/ru/labs/flights/AirportMapper.java
+++ src/main/java/ru/labs/flights/AirportMapper.java
@@ -3,6 +3,9 @@ package ru.labs.flights;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
+    @Override
+    protected void map()
 }
