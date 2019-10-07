15af8c92bcb553de2fe0616e0c5f6367e8a237cb
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 19:16:35 +0300

    Scripted auto-commit on change (2019-10-07 19:16:35) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightReduceJoin.java
+++ src/main/java/ru/labs/flights/FlightReduceJoin.java
@@ -4,11 +4,13 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class FlightReduceJoin extends Reducer<ReduceSideJoinKey, Text, Text, Text> {
     @Override
     protected void reduce(ReduceSideJoinKey key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        
+        Iterator<Text> iter = values.iterator();
+        Text airportName = new Text(iter.next());
     }
 }
