f8c8de96d9efc117c447c73794790fccc7f4bbcb
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 20:49:06 +0300

    Scripted auto-commit on change (2019-10-07 20:49:06) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightReduceJoin.java
+++ src/main/java/ru/labs/flights/FlightReduceJoin.java
@@ -31,7 +31,7 @@ public class FlightReduceJoin extends Reducer<ReduceSideJoinKey, Text, Text, Tex
         }
 
         if (delayCount != 0) {
-            context.write(airportName, new Text((summaryDelay / delayCount)+","+maxDelay+","+minDelay));
+            context.write(airportName, new Text(testStr);
             //context.write(airportName, new Text((summaryDelay / delayCount)+","+maxDelay+","+minDelay));
         }
     }
