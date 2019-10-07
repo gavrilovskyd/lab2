8d6a2b2ed985663d1472e3aae8720b162c532e08
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 22:01:24 +0300

    Scripted auto-commit on change (2019-10-07 22:01:24) by gitwatch.sh

--- src/main/java/ru/labs/flights/ReduceSideJoinKey.java
+++ src/main/java/ru/labs/flights/ReduceSideJoinKey.java
@@ -40,6 +40,7 @@ public class ReduceSideJoinKey implements WritableComparable<ReduceSideJoinKey> 
 
     public int compareTo(ReduceSideJoinKey k) {
         int keyResult = joinKey.compareTo(k.joinKey);
+        System.out.println(joinKey+" "+isUnique+" COMPARE "+k.joinKey+" "+k.isUnique);
         return (keyResult == 0 ? (k.isUnique - isUnique) : keyResult);
     }
 
