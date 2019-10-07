fa9258c5a1a9f2a0be524b985512cc6e04a5814f
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 18:10:59 +0300

    Scripted auto-commit on change (2019-10-07 18:10:59) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightsJoinApp.java
+++ src/main/java/ru/labs/flights/FlightsJoinApp.java
@@ -1,6 +1,7 @@
 package ru.labs.flights;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -18,5 +19,7 @@ public class FlightsJoinApp {
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new);
     }
 }
