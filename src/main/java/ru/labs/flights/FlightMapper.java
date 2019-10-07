6da36190063cedfa90c9431d1540314d680ed250
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 18:03:49 +0300

    Scripted auto-commit on change (2019-10-07 18:03:49) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
+public class FlightMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         // TODO: read csv correct
