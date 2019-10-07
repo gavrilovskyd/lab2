346633928ca795452dfbd5efe0a45b44b01464a8
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 16:11:46 +0300

    Scripted auto-commit on change (2019-10-07 16:11:46) by gitwatch.sh

--- src/main/java/ru/labs/flights/ReduceSideJoinKey.java
+++ src/main/java/ru/labs/flights/ReduceSideJoinKey.java
@@ -2,7 +2,14 @@ package ru.labs.flights;
 
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class ReduceSideJoinKey implements Writable {
     private int joinKey;
     private boolean isUnique;
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt();
+    }
 }
