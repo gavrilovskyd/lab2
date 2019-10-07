a6dd52983abe7e08afe47ade4e368009e79c66be
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 19:20:18 +0300

    Scripted auto-commit on change (2019-10-07 19:20:18) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightReduceJoin.java
+++ src/main/java/ru/labs/flights/FlightReduceJoin.java
@@ -19,6 +19,10 @@ public class FlightReduceJoin extends Reducer<ReduceSideJoinKey, Text, Text, Tex
         float minDelay = Float.MAX_VALUE;
         while (iter.hasNext()) {
             float delay = Float.parseFloat(iter.next().toString());
+            delayCount++;
+            summaryDelay += delay;
+            maxDelay = Math.max(maxDelay, delay);
+            minDelay = Math.min(minDelay, delay);
         }
     }
 }
