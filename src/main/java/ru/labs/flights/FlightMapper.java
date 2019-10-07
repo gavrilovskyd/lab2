93b6d6657153d80aee1bd58c4206e158c81bb39e
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 18:03:38 +0300

    Scripted auto-commit on change (2019-10-07 18:03:38) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -1,4 +1,21 @@
 package ru.labs.flights;
 
-public class FlightMapper {
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // TODO: read csv correct
+        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
+        CSVRecord record = parser.getRecords().get(0); //TODO: catch
+
+        context.write(new ReduceSideJoinKey(Integer.parseInt(record.get(0)), true), new Text(record.get(1)));
+    }
 }
