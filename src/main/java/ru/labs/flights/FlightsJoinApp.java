7fe774dc5e5362caa039c65df364e5e75c3515f1
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 14:07:37 +0300

    Scripted auto-commit on change (2019-10-07 14:07:37) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightsJoinApp.java
+++ src/main/java/ru/labs/flights/FlightsJoinApp.java
@@ -2,6 +2,9 @@ package ru.labs.flights;
 
 public class FlightsJoinApp {
     public static void main(String []args) throws Exception {
-        
+        if (args.length != 2) {
+            System.err.println("Usage: FlightsJoinApp <input path> <output path>");
+            System.exit(-1);
+        }
     }
 }
