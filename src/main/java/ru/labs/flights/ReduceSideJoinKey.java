47914bf993edb6242e8c79e65d36ffe6a9ff673f
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 21:26:01 +0300

    Scripted auto-commit on change (2019-10-07 21:26:00) by gitwatch.sh

--- src/main/java/ru/labs/flights/ReduceSideJoinKey.java
+++ src/main/java/ru/labs/flights/ReduceSideJoinKey.java
@@ -38,9 +38,9 @@ public class ReduceSideJoinKey implements WritableComparable<ReduceSideJoinKey> 
         return k;
     }
 
-    public int compareTo(ReduceSideJoinKey k) throws Exception {
+    public int compareTo(ReduceSideJoinKey k) {
         int keyResult = joinKey.compareTo(k.joinKey);
-        throw new Exception("debug pro level");
+
         return (keyResult == 0 ? (isUnique - k.isUnique) : keyResult);
     }
 
