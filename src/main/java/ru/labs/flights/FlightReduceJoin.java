9aa478ae4146662370d085d6a0c1a87b3cf0b8bd
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 19:18:10 +0300

    Scripted auto-commit on change (2019-10-07 19:18:10) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightReduceJoin.java
+++ src/main/java/ru/labs/flights/FlightReduceJoin.java
@@ -13,7 +13,9 @@ public class FlightReduceJoin extends Reducer<ReduceSideJoinKey, Text, Text, Tex
         Iterator<Text> iter = values.iterator();
         Text airportName = new Text(iter.next());
 
-        float avgDelay = 0;
+        float summaryDelay = 0;
+
+
         while (iter.hasNext()) {
 
         }
