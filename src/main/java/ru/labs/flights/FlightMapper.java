c40668fe7864b59942803a843024e3d88122cd06
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 18:39:19 +0300

    Scripted auto-commit on change (2019-10-07 18:39:19) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -16,7 +16,6 @@ public class FlightMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, 
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
         CSVRecord record = parser.getRecords().get(0); //TODO: catch
 
-        context.write(new ReduceSideJoinKey(new Text(record.get(14)), true),
-                new Text(record.get(18)));
+        context.write(new ReduceSideJoinKey(new Text(record.get(14)), true), new Text(record.get(18)));
     }
 }
