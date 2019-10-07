d7a01c318948c06611652c1fddaac39632a5868d
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 17:05:45 +0300

    Scripted auto-commit on change (2019-10-07 17:05:45) by gitwatch.sh

--- src/main/java/ru/labs/flights/AirportMapper.java
+++ src/main/java/ru/labs/flights/AirportMapper.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+
     }
 }
