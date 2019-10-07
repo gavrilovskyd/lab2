863bf195c6307d7bd1bc70bdbabc374c8726ed77
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 21:46:51 +0300

    Scripted auto-commit on change (2019-10-07 21:46:51) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightsJoinApp.java
+++ src/main/java/ru/labs/flights/FlightsJoinApp.java
@@ -25,13 +25,13 @@ public class FlightsJoinApp {
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(HashPartitioner.class);
-        job.setGroupingComparatorClass(ReduceSideJoinKey.GroupingComparator.class);
-        job.setReducerClass(FlightReduceJoin.class);
+        //job.setGroupingComparatorClass(WritableComparator.class);
+        //job.setReducerClass(FlightReduceJoin.class);
         job.setMapOutputKeyClass(ReduceSideJoinKey.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
+        //job.setOutputKeyClass(Text.class);
+        //job.setOutputValueClass(Text.class);
+        //job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
