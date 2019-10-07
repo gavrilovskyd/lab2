0a98255e8c33ca31c24a2822691ffd8081fa21c7
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 21:19:55 +0300

    Scripted auto-commit on change (2019-10-07 21:19:54) by gitwatch.sh

--- src/main/java/ru/labs/flights/ReduceSideJoinKey.java
+++ src/main/java/ru/labs/flights/ReduceSideJoinKey.java
@@ -48,6 +48,8 @@ public class ReduceSideJoinKey implements WritableComparable<ReduceSideJoinKey> 
     }
 
     public static class GroupingComparator extends WritableComparator {
+
+        @Override
         public int compare(ReduceSideJoinKey key1, ReduceSideJoinKey key2) {
             return key1.compareTo(key2);
         }
