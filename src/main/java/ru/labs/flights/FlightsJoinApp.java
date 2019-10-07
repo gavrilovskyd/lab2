9c70f30a721ce3dc1c47173eaebd2d5082e73c8c
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-07 18:06:11 +0300

    Scripted auto-commit on change (2019-10-07 18:06:11) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightsJoinApp.java
+++ src/main/java/ru/labs/flights/FlightsJoinApp.java
@@ -1,10 +1,14 @@
 package ru.labs.flights;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class FlightsJoinApp {
     public static void main(String []args) throws Exception {
         if (args.length != 2) {
             System.err.println("Usage: FlightsJoinApp <input path> <output path>");
             System.exit(-1);
         }
+
+        Job job = Job.getInstance();
     }
 }
