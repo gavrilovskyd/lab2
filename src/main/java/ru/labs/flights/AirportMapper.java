ca041413eb01ac13fbbc2f583164630831c0908a
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-08 08:58:40 +0300

    Scripted auto-commit on change (2019-10-08 08:58:40) by gitwatch.sh

--- src/main/java/ru/labs/flights/AirportMapper.java
+++ src/main/java/ru/labs/flights/AirportMapper.java
@@ -10,6 +10,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
+    private static final String[] flightHeader = {
+            "YEAR","QUARTER","MONTH", "DAY_OF_MONTH","DAY_OF_WEEK","FL_DATE","UNIQUE_CARRIER",
+            "AIRLINE_ID","CARRIER","TAIL_NUM","FL_NUM","ORIGIN_AIRPORT_ID","ORIGIN_AIRPORT_SEQ_ID",
+            "ORIGIN_CITY_MARKET_ID","DEST_AIRPORT_ID","WHEELS_ON","ARR_TIME","ARR_DELAY","ARR_DELAY_NEW",
+            "CANCELLED","CANCELLATION_CODE","AIR_TIME","DISTANCE"
+    };
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader("Code", "Description"));
