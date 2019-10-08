4715dcd752ead67ea984a2fbd5442f46ee3d1a62
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-08 08:51:09 +0300

    Scripted auto-commit on change (2019-10-08 08:51:09) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -19,7 +19,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, 
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader());
+        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader(flightHeader));
         CSVRecord record = parser.getRecords().get(0); //TODO: catch
 
         if (Float.parseFloat(record.get(19)) < 1e-6             // Not canceled
