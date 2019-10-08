12c0981234e239bb1b0e26757ab1fa7aae2003f2
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-08 08:51:02 +0300

    Scripted auto-commit on change (2019-10-08 08:51:02) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -14,7 +14,8 @@ public class FlightMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, 
             "YEAR","QUARTER","MONTH", "DAY_OF_MONTH","DAY_OF_WEEK","FL_DATE","UNIQUE_CARRIER",
             "AIRLINE_ID","CARRIER","TAIL_NUM","FL_NUM","ORIGIN_AIRPORT_ID","ORIGIN_AIRPORT_SEQ_ID",
             "ORIGIN_CITY_MARKET_ID","DEST_AIRPORT_ID","WHEELS_ON","ARR_TIME","ARR_DELAY","ARR_DELAY_NEW",
-            "CANCELLED","CANCELLATION_CODE","AIR_TIME","DISTANCE"};
+            "CANCELLED","CANCELLATION_CODE","AIR_TIME","DISTANCE"
+    };
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
