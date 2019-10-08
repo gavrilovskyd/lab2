cb09d3ac61c64cb1cdf225138eaf982a9ba2e8ef
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-08 08:50:07 +0300

    Scripted auto-commit on change (2019-10-08 08:50:07) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightMapper.java
+++ src/main/java/ru/labs/flights/FlightMapper.java
@@ -10,12 +10,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, ReduceSideJoinKey, Text> {
+    static final String[] flightHeader = {"YEAR","QUARTER","MONTH",
+            "DAY_OF_MONTH","DAY_OF_WEEK","FL_DATE","UNIQUE_CARRIER","AIRLINE_ID","CARRIER","TAIL_NUM","FL_NUM",
+            "ORIGIN_AIRPORT_ID","ORIGIN_AIRPORT_SEQ_ID","ORIGIN_CITY_MARKET_ID","DEST_AIRPORT_ID","WHEELS_ON",
+            "ARR_TIME","ARR_DELAY","ARR_DELAY_NEW","CANCELLED","CANCELLATION_CODE","AIR_TIME","DISTANCE"};
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader("YEAR","QUARTER","MONTH",
-                "DAY_OF_MONTH","DAY_OF_WEEK","FL_DATE","UNIQUE_CARRIER","AIRLINE_ID","CARRIER","TAIL_NUM","FL_NUM",
-                "ORIGIN_AIRPORT_ID","ORIGIN_AIRPORT_SEQ_ID","ORIGIN_CITY_MARKET_ID","DEST_AIRPORT_ID","WHEELS_ON",
-                "ARR_TIME","ARR_DELAY","ARR_DELAY_NEW","CANCELLED","CANCELLATION_CODE","AIR_TIME","DISTANCE"));
+        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader());
         CSVRecord record = parser.getRecords().get(0); //TODO: catch
 
         if (Float.parseFloat(record.get(19)) < 1e-6             // Not canceled
