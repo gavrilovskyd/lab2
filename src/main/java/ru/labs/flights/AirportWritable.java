f4b1bab12be20274bafb7d1c47c06f40df86bca0
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-11 23:57:56 +0300

    Scripted auto-commit on change (2019-10-11 23:57:56) by gitwatch.sh

--- src/main/java/ru/labs/flights/AirportWritable.java
+++ src/main/java/ru/labs/flights/AirportWritable.java
@@ -1,7 +1,8 @@
 package ru.labs.flights;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 public class AirportWritable implements Writable {
-    
+    private Text csvLine;
 }
