97af2dbc8d31a6fa1b50e2093c9ffb6e87a16681
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-11 23:59:13 +0300

    Scripted auto-commit on change (2019-10-11 23:59:13) by gitwatch.sh

--- src/main/java/ru/labs/flights/AirportWritable.java
+++ src/main/java/ru/labs/flights/AirportWritable.java
@@ -3,10 +3,18 @@ package ru.labs.flights;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInput;
+import java.io.IOException;
+
 public class AirportWritable implements Writable {
     private Text csvLine;
 
     public AirportWritable() {
         csvLine = new Text();
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        
     }
 }
